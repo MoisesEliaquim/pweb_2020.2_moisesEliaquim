f5581116a20883d10cde02d42a79c5ffdc17a2ce
Melkim <monca.moises@gmail.com>
2021-04-10 14:10:49 -0300

    STS
    STS

---
+++ cadpessoas_moises/src/main/java/br/com/moises/cadpessoas/CadpessoasMoisesApplication.java
@@ -0,0 +1,13 @@
+package br.com.moises.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasMoisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasMoisesApplication.class, args);
+	}
+
+}
---
+++ cadpessoas_moises/src/test/java/br/com/moises/cadpessoas/CadpessoasMoisesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moises.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasMoisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
