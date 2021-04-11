3eb64d305ffa1ff91f623315b158c5ddc9f9bf89
Melkim <monca.moises@gmail.com>
2021-04-11 04:34:50 -0300

    MVC UPD 1
    Whitelabel Error Page por todo lado.

--- cadpessoas_moisesEliaquim/src/main/java/br/com/moisesEliaquim/cadpessoas/controller/PessoaController.java
+++ cadpessoas_moisesEliaquim/src/main/java/br/com/moisesEliaquim/cadpessoas/controller/PessoaController.java
@@ -1,10 +1,33 @@
 package br.com.moisesEliaquim.cadpessoas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.moisesEliaquim.cadpessoas.Repository.PessoaRepository;
+import br.com.moisesEliaquim.cadpessoas.model.Pessoa;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
 
+	@Autowired
+	PessoaRepository pessoaRepo;
+	
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
+	
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+		List<Pessoa> lista = pessoaRepo.findAll();
+		ModelAndView mav = new ModelAndView("listarPessoas");
+		mav.addObject("pessoas", lista);
+		return mav;
+	}
 }
