18520344279f679e0914dcec5ed229947f7dcd50
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2021-05-03 04:18:53 -0300

    Arrumando pastas e arquivos.

---
+++ cadpessoas_moisesEliaquim2/src/main/java/br/com/moisesEliaquim/cadpessoas/CadpessoasMoisesEliaquimApplication.java
@@ -0,0 +1,13 @@
+package br.com.moisesEliaquim.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasMoisesEliaquimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasMoisesEliaquimApplication.class, args);
+	}
+
+}
---
+++ cadpessoas_moisesEliaquim2/src/test/java/br/com/moisesEliaquim/cadpessoas/CadpessoasMoisesEliaquimApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moisesEliaquim.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasMoisesEliaquimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
