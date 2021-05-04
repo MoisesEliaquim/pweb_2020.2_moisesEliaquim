3fca4c116f59b53ad479a2dda5007dae16c5391e
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2021-05-04 02:42:52 -0300

    Update Pessoa.java

--- u1_exercicio3_1/src/main/java/br/com/moisesEliaquim/cadpessoas/model/Pessoa.java
+++ u1_exercicio3_1/src/main/java/br/com/moisesEliaquim/cadpessoas/model/Pessoa.java
@@ -20,45 +20,51 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name="Pessoas")
-public class Pessoa implements Serializable{
-	
+@Table(name = "pessoas")
+public class Pessoa implements Serializable {
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nome;
 	private String genero;
 	private String email;
-	
+
 	@Column(nullable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate dataNascimento;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public LocalDate getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(LocalDate dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
-	
-	
+
 }
